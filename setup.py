6d970a351a596ab4ec86a50f1ae74aa5aa271d2d
AMIT NEGI <negia2091@gmail.com>
2025-03-31 19:49:46 +0530

    this is my first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='scrapper',
+    version='0.0.1',
+    author='amitnegi',
+    author_email='negia2091@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
